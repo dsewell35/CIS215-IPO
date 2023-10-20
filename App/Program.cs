4bea13c9ce794b582e5a646bb9c86b789266cdea
dsewell35 <143026340+dsewell35@users.noreply.github.com>
2023-10-20 16:52:47 -0500

    Final commit and push?

--- App/Program.cs
+++ App/Program.cs
@@ -18,6 +18,8 @@ namespace App
             int WordCnt, CharCnt, NumCnt, SpecCnt;
             /********************** DO NOT EDIT ABOVE THIS LINE **********************************/
 
+
+            //Variable Initialization
             int lettrCnt = 0;
             int sentCnt = 0;
             double wrdsPrCnt = 0;
@@ -29,6 +31,13 @@ namespace App
 
 
 
+            ///<summary>
+            ///This block takes the users input and counts the number
+            ///Of Characters in the input by stepping through it as an array
+            ///And also assigns the var variable c to each character input in the array
+            ///The if loop then compares the variable to known values in order
+            ///To increase specific counts
+            /// </summary>
             for (int i = 0; i < inp.Length; i++)
             {
                 var c = inp[i];
@@ -46,12 +55,25 @@ namespace App
                     SpecCnt++;
                 }
             }
-
+            ///<summary>
+            ///This block parses the user input
+            ///Then splits the string into a string array
+            ///At each whitespace character
+            /// The for loop then steps through the array
+            /// Counting the number of words in the array
+            /// </summary>
             string[] words = inp.Split(null);
             for (int i = 0; i < words.Length; i++)
             {
                 WordCnt++;
             }
+
+            ///<summary>
+            ///This block delimits sentence ending characters
+            ///then splits the string where the delimiters end
+            ///The For loop then steps through the array
+            ///Adding to the total of sentence count with each iteration
+            /// <summary>
 
 
             char[] delimit = { '.', '!', '?' };
@@ -61,23 +83,20 @@ namespace App
                 sentCnt++;
             }
 
+            //Calculations
             wrdsPrCnt = (double)WordCnt / (double)sentCnt;
+            charPrCnt = (double)CharCnt / (double)sentCnt;
 
-
+            //Output section
             Console.WriteLine("Word Count: " + WordCnt);
             Console.WriteLine("Sentence Count: " + sentCnt);
-            Console.WriteLine("Words Per Sentence: " + string.Format("{0:0.000}", wrdsPrCnt));
+            Console.WriteLine("Words Per Sentence: " + string.Format("{0:0.000}", wrdsPrCnt)); //Narrows percision to 3 places
             Console.WriteLine("Character Count: " + CharCnt);
             Console.WriteLine("Special Character Count: " + SpecCnt);
-            Console.WriteLine("Characters Per Sentence: " + string.Format("{0:0.0000}", charPrCnt));
+            Console.WriteLine("Characters Per Sentence: " + string.Format("{0:0.0000}", charPrCnt)); //Narrows percision to 4 places
             Console.WriteLine("Number Count: " + NumCnt);
 
 
-
-            Console.WriteLine(SpecCnt);
-            Console.WriteLine(lettrCnt);
-            Console.WriteLine(WordCnt);
-            Console.WriteLine(sentCnt);
 
 
             // THIS SHOULD BE THE LAST STATEMENT FOR MAIN
